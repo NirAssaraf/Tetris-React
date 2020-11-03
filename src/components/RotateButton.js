fc263877b7653ed34ec5131a8eac87975a63fb89
NirAssaraf <assarafnir@gmail.com>
2020-11-03 21:47:45 +0200

    change 2

--- src/components/RotateButton.js
+++ src/components/RotateButton.js
@@ -5,9 +5,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const StyledRButton = styled.button`
 box-sizing: border-box;
-width: 20vmin;
+// width: 20vmin;
+width: "100%";
 margin: 0 0 5px 30px;
-padding: 20px;
+// padding: 20px;
 min-height: 30px;
 border-radius:20px;
 border: none;
@@ -18,6 +19,8 @@ font-size: 1rem;
 outline: none;
 cursor: pointer;
 display: flex;
+aspectRatio: 10 / 3;
+padding: "8rem";
 `;
 
 const RotateArrowButton = ({ callback }) => (
